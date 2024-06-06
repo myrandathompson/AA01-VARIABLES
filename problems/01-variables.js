3f225ebf3718f02bb3b540b9763d86fc7e599490
myrandathompson <myrandathompson@icloud.com>
2024-06-06 16:15:16 -0700

    added firstName, lastName, age

--- problems/01-variables.js
+++ problems/01-variables.js
@@ -13,29 +13,34 @@ let firstName = 'Myranda';
 //    as a string:
 
 // Your code here 
-
+let lastName = "Thompson";
 
 // 3. Declare a variable called `age`:
 
 // Your code here 
-
+let age;
 
 // 4. Print out the `firstName`, `lastName`, and `age` variables. What
 //    do you expect them to be when they get printed?
-
+// Myranda
+// Thompson
+// undefined
 // Your code here 
-
-
+console.log(firstName);
+console.log(lastName);
+console.log(age);
 // 5. Assign the `age` variable to a number:
 
 // Your code here 
-
+age = 32;
 
 // 6. Print out the `firstName`, `lastName`, and `age` variables. What
 //    do you expect them to be when they get printed?
 
 // Your code here 
-
+console.log(firstName);
+console.log(lastName);
+console.log(age);
 
 /******************** DO NOT MODIFY ANY CODE BELOW THIS LINE *****************/
 const obj = {};
